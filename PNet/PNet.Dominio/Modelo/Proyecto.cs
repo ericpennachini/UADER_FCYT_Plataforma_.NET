8606513676582fe8164591933257d1cf5113b0ad
Eric Pennachini <eric.pennachini@gmail.com>
2015-06-18 01:08:31 -0300

    MVC
    Se agrego un proyecto ASP .NET MVC para la capa de presentacion,
    haciendo ABM de Proyecto.

--- PNet/PNet.Dominio/Modelo/Proyecto.cs
+++ PNet/PNet.Dominio/Modelo/Proyecto.cs
@@ -11,6 +11,7 @@ namespace PNet.Dominio.Modelo
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public partial class Proyecto
     {
